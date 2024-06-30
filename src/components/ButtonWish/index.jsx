18b0a02d94186c43c0d8740c626c2ea3f3712ac1
GUSTAVO.NEVES <gustavo.neves@infracommerce.com.br>
2024-06-30 15:42:42 -0300

    ajustando isFavorite no ButtonWish

--- src/components/ButtonWish/index.jsx
+++ src/components/ButtonWish/index.jsx
@@ -1,23 +1,11 @@
-import { useState, useEffect } from "react";
 import './global.css'
 
 const ButtonWish = ({ isFavorite, onToggleWish }) => {
-  const [fav, setFav] = useState(isFavorite);
-
-  useEffect(() => {
-    setFav(isFavorite);
-  }, [isFavorite]);
-
-  const toggleWish = () => {
-    setFav(!fav);
-    onToggleWish();
-  };
-
   return (
     <>
       <button
-        className={"product-wish-btn" + (fav ? "--active" : "")}
-        onClick={() => toggleWish(!fav)}
+        className={"product-wish-btn" + (isFavorite ? "--active" : "")}
+        onClick={() => onToggleWish()}
       />
     </>
   );
